9adde7573044b3daf47f4944fe9df111945fae2e
prdxn88 <omkarv@axioned.com>
2023-09-15 15:44:25 +0530

    key added

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,9 @@ export default async function Home() {
      <h2 className="text-center mb-4 text-2xl font-bold">Posts</h2>
       <ul className="flex flex-col gap-y-5 md:flex-row gap-x-[5%] w-full">
         {
-          posts.map((post)=>{
+          posts.map((post,i)=>{
             return (
-             <PostCard post={post}/>
+             <PostCard post={post} key={i}/>
             )
           })
         }
